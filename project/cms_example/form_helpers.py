cdfedee5a1bb10d3759a6414f389bfafe4b003fc
fivethreeo <oyvind.saltvik@gmail.com>
2011-08-09 18:19:16 +0200

    parent ok now?

--- project/cms_example/form_helpers.py
+++ project/cms_example/form_helpers.py
@@ -3,24 +3,20 @@ from uni_form.helpers import FormHelper, Submit, Reset
 from uni_form.helpers import Layout, Fieldset, Row, HTML
 
 comment_form_helper = FormHelper()
-"""
-# create the layout object
-commentform_layout = Layout(
-    # first fieldset shows the company
-    Fieldset('', 'is_company'),
 
-    # second fieldset shows the contact info
-    Fieldset('Contact details',
-        HTML(style),
+comment_form_layout = Layout(
+    Fieldset('',
+        'parent',
+        'name'
         'email',
-        Row('password1','password2'),
-        'first_name',
-        'last_name',
+        'url'
+        'title',
+        'comment'
     )
 )
 
-commentform_helper.add_layout(commentform_layout)
-"""
+commentform_helper.add_layout(comment_form_layout)
+
 comment_form_helper.form_action = reverse('comments-post-comment')
 comment_form_helper.form_method = 'POST'
 comment_form_helper.add_input(Submit('post','Post'))
